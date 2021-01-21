a4d5ecaa0f31c2c732a5f604c91f3aa718b3e89d
johninkorea <john829829@gmail.com>
2021-01-21 19:20:50 +0900

    krean-->english, add comment

--- YMD2.py
+++ YMD2.py
@@ -2,16 +2,12 @@
 import math
 import numpy as np
 
-def df (a):
-    s=a//1
-    return s
-
-
-
+#input list 
 YMD=[]
 
+# Separate symbols and letters.
 while True:
-    YMD[0:]=input("년도와 월 일을 순서대로 입력").split()
+    YMD[0:]=input("Please enter the year and month dates in order. (Separated by Spaces)").split()
     
     y0=YMD[0].isdigit()              
     z0=YMD[0].isalpha()
@@ -23,55 +19,57 @@ while True:
     y=y0 or y1 or y2
     z=z0 or z1 or z2
     
-    if y==True and z==False:                  #숫자를
+    if y==True and z==False:   # break when all of input are numbers.
        break
     
     else:
-        print("정수로 입력해야합니다.")
-        
+        print("you have to enter numbers")   # print errer message
+
+# take input to variable.
 Y=int(YMD[0])
 M=int(YMD[1])
 D=int(YMD[2])
 
 
 
-
-e=df((M+9)/12)
-f=df(7*(Y+e)/4)
-g=df((275*M)/9)
+# calculate Julian Day
+e=((M+9)/12)//1
+f=(7*(Y+e)/4)//1
+g=((275*M)/9)//1
 h=np.sign(100*Y+M-190002.5)
 JD=(367*Y-f+g+D+1721013.5-0.5*h+0.5)
-W=JD%7//1
 
-day_of_the_week={1.:'수', 2.:'목', 3.:'금', 4.:'토', 5.:'일', 6.:'월', 0.:'화'}
+# calculate day of week
+W=JD%7//1    
+DFW={1.:'wednes', 2.:'Thurs', 3.:'fri', 4.:'satur', 5.:'sun', 6.:'mon', 0.:'tues'}
+W=DFW[W]
 
-W=day_of_the_week[W]
-    
-print(f"해당 날은 {W}요일입니다.")
+# print Day of week    
+print(f"That day is {W}day.")
 
 
+# calculate first day of month
 JD0=(367*Y-f+g+1+1721013.5-0.5*h+0.5)
 W0=JD0%7//1
-print(f"입력한 날짜에 해당하는 달은 {W}요일에 시작합니다.")
+print(f"The month that corresponds to the date you entered begins on {W0}day.")
 
-###########################################################
-#윤년 계산
 
-m=[31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+# calculate if that year are leap year. 
+m=[31, 0, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
 if Y/400%1==0:
     m[1]=29
-    print("해당년은 윤년이며")
+    print("The year is the leap year.")
 elif Y/100%1==0:
     m[1]=28
-    print("해당년은 평년이며")
+    print("The year is the average year.")
 elif Y/4%1==0:
     m[1]=29
-    print("해당년은 윤년이며")
+    print("The year is the leap year.")
 else:
     m[1]=28
-    print("해당년은 평년이며")
+    print("The year is the average year.")
 
-print("해당 년에 월별 일수",m)
+print("Days per month in the year:",m)
 
 
 #[일 월   화   수   목   금   토]
@@ -84,24 +82,24 @@ else:
     W0=W0+2
 
 
-print("\n","*"*40,"\n해당 날짜의 달력을 출력합니다\n일\t월\t화\t수\t목\t금\t토")
+
+# print reult
+print("\n","*"*40,"\nPrint calendar\nsun\tmon\ttue\twed\tthur\tfri\tsat")
+
+
+# add zeros emty space
 MF=[]
 z=0
 while z<W0:
     MF.insert(z,0)
     z+=1
 
-
+# make day lost in one line
 x=1
 while x<m[M-1]+1:
     MF.append(x)
     x+=1
 
-
-
-#리스트를 해당월에대해 시작요일 부터 숫자를 채워서 마지막까지 한번에 리스트를 만들고
-#일정하개 7번째 마다 줄을 띄울수있다면?
-#리스트를 만들때 insert하던가 m[0:a]=insert(0), m[a:]insert(x)-->whil ansdmfh m[a]몃번째 달까지해서 대입쭉
 
 for k,v in enumerate(range(len(MF)),1):
     print("{}\t".format(MF[v]), end='\n' if k%7==0 else '  ' )
